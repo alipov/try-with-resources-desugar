08a326191d8a2f4b765e9b551eec033455e77b79
Alex Lipov <tech.alexl@gmail.com>
2019-09-25 09:56:08 +0300

    Moving invocation logic into Sample class.

--- app/src/main/java/info/osom/trywithresources/Sample.java
+++ app/src/main/java/info/osom/trywithresources/Sample.java
@@ -1,10 +1,34 @@
 package info.osom.trywithresources;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
 import java.io.File;
 import java.io.IOException;
 
 class Sample {
-    static void tryWithResourcesSample(File file) throws IOException {
+    static void run(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            invokeSample(context);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static void invokeSample(Context context) {
+        try {
+            Sample.tryWithResourcesSample(new File(context.getFilesDir(), "hello.txt"));
+        } catch (IOException e) {
+            Log.d("TRY", "caught exception: " + e.getMessage());
+            Throwable[] suppressed = e.getSuppressed();
+            for (Throwable throwable : suppressed) {
+                Log.d("TRY", "suppressed exception: " + throwable.getMessage());
+            }
+        }
+    }
+
+    private static void tryWithResourcesSample(File file) throws IOException {
         try (MyFileOutputStream os = new MyFileOutputStream(file)) {
             os.write("hello".getBytes());
             throw new IOException("thrown in try block");
